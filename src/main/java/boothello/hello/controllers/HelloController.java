3166cfa6a9611adc7e359db500f87d002c5fb82f
ppliu <ppliu1987@126.com>
2018-07-10 15:41:30 +0800

    commit project

---
+++ src/main/java/boothello/hello/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package boothello.hello.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("hello")
+	public String hello(){
+		return "Hello-World";
+	}
+}
